74c7acead66f8a35217d10c756a3ab2305214125
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-11-27 20:29:18 +0800

    updated the app logo

--- resources/js/Layouts/App.vue
+++ resources/js/Layouts/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { Button } from "@/components/ui/button";
+import Logo from "../../images/logo.png";
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -71,9 +71,9 @@ const logout = () => {
                 <div
                     class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6"
                 >
-                    <a href="/" class="flex items-center gap-2 font-semibold">
-                        <Package2 class="h-6 w-6" />
-                        <span class="">Project Story</span>
+                    <a href="/" class="flex items-center font-semibold">
+                        <img :src="Logo" alt="logo" class="size-20" />
+                        <span class="font-bold">Project David</span>
                     </a>
                     <Button
                         variant="outline"
--- resources/js/Pages/ReceivingApproval/Show.vue
+++ resources/js/Pages/ReceivingApproval/Show.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useBackButton } from "@/Composables/useBackButton";
+import Checkbox from "primevue/checkbox";
 const { backButton } = useBackButton(route("receiving-approvals.index"));
 const props = defineProps({
     order: {
@@ -7,13 +8,28 @@ const props = defineProps({
         required: true,
     },
 });
+
+const selectedItems = ref([]);
+
+const bulkApprove = () => {
+    console.log(selectedItems.value);
+};
 </script>
 
 <template>
     <Layout :heading="`Order Number ${order.order_number}`">
         <TableContainer>
+            <TableHeader>
+                <Button
+                    v-if="selectedItems.length > 0"
+                    @click="bulkApprove"
+                    variant="outline"
+                    >Approve Selected Items</Button
+                >
+            </TableHeader>
             <Table>
                 <TableHead>
+                    <TH> </TH>
                     <TH>Item</TH>
                     <TH>Inventory Code</TH>
                     <TH>Order Date</TH>
@@ -23,6 +39,13 @@ const props = defineProps({
                 </TableHead>
                 <TableBody>
                     <tr v-for="item in order.store_order_items" :key="item.id">
+                        <TD>
+                            <Checkbox
+                                v-model="selectedItems"
+                                :value="item.id"
+                                :inputId="`item-${item.id}`"
+                            />
+                        </TD>
                         <TD>{{ item.product_inventory.name }}</TD>
                         <TD>{{ item.product_inventory.inventory_code }}</TD>
                         <TD>{{ order.order_date }}</TD>
