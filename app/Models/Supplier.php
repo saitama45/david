c2dd36278144e9228100fd7f4af3e7bb440c2302
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-11-29 07:23:27 +0800

    Fixed ui issue

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\HasSelections;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Supplier extends Model
 {
@@ -21,5 +22,10 @@ class Supplier extends Model
     public function store_orders()
     {
         return $this->hasMany(StoreOrder::class);
+    }
+
+    public function scopeOptions(Builder $query)
+    {
+        return $query->where('is_active', true)->pluck('name', 'id');
     }
 }
--- resources/js/Layouts/App.vue
+++ resources/js/Layouts/App.vue
@@ -64,9 +64,9 @@ const logout = () => {
     <Toast />
     <ConfirmDialog></ConfirmDialog>
     <div
-        class="grid min-h-screen max-h-screen overflow-hidden w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]"
+        class="grid min-h-screen max-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]"
     >
-        <div class="hidden border-r bg-muted/40 md:block overflow-auto">
+        <div class="hidden border-r bg-muted/40 md:block">
             <div class="flex h-full max-h-screen flex-col gap-2">
                 <div
                     class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6"
