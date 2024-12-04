2de9748a3d865d52e88f58053d6e5ca425b3b451
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-12-04 16:32:55 +0800

    updated the algorithnm for generating order number

--- app/Http/Controllers/StoreOrderController.php
+++ app/Http/Controllers/StoreOrderController.php
@@ -226,9 +226,16 @@ class StoreOrderController extends Controller
     public function getOrderNumber($id)
     {
         $branchId = $id;
-        $branchCode = StoreBranch::select('branch_code')->findOrFail($branchId)->branch_code;
-        $orderCount = StoreOrder::where('store_branch_id', $branchId)->count() + 1;
-        $orderNumber = str_pad($orderCount, 5, '0', STR_PAD_LEFT);
-        return "$branchCode-$orderNumber";
+        $count = 1;
+        while (true) {
+            $branchCode = StoreBranch::select('branch_code')->findOrFail($branchId)->branch_code;
+            $orderCount = StoreOrder::where('store_branch_id', $branchId)->count() + $count;
+            $orderNumber = str_pad($orderCount, 5, '0', STR_PAD_LEFT);
+            $store_order_number = "$branchCode-$orderNumber";
+            $result = StoreOrder::where('order_number', $store_order_number)->first();
+            $count++;
+            if (!$result) break;
+        }
+        return $store_order_number;
     }
 }
