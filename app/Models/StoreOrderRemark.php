6436160683765cc01dbe1bc707e09711ebaa815f
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-12-09 15:30:58 +0800

    added Store order remark model

---
+++ app/Models/StoreOrderRemark.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StoreOrderRemark extends Model
+{
+    /** @use HasFactory<\Database\Factories\StoreOrderRemarkFactory> */
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'order_id',
+        'action',
+        'remarks'
+    ];
+}
