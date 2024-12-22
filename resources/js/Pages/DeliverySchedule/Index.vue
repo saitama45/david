f41d07d6fe2c4d88def2b9527a1a1ace138bef88
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-12-22 13:38:44 +0800

    added table on delivery schedule index page

--- resources/js/Pages/DeliverySchedule/Index.vue
+++ resources/js/Pages/DeliverySchedule/Index.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+
+</script>
 
 <template>
     <Layout heading="Delivery Schedules">
@@ -8,7 +10,7 @@
                     <TH>Store Branch</TH>
                     <TH>Loc Code</TH>
                     <TH>
-                        <DivFlexCol class="justify-center items-center">
+                        <DivFlexCol>
                             Normal Orders
                             <LabelXS class="text-[10px]"
                                 >Sun (7:00 am) to Wed (7:00 am)</LabelXS
@@ -16,7 +18,7 @@
                         </DivFlexCol>
                     </TH>
                     <TH>
-                        <DivFlexCol class="justify-center items-center">
+                        <DivFlexCol>
                             Normal Orders
                             <LabelXS class="text-[10px]"
                                 >Thurs (7:00 am) to Sat (7:00 am)</LabelXS
@@ -24,19 +26,19 @@
                         </DivFlexCol>
                     </TH>
                     <TH>
-                        <DivFlexCol class="justify-center items-center">
+                        <DivFlexCol>
                             DTS Orders
                             <LabelXS class="text-[10px]">Ice Cream</LabelXS>
                         </DivFlexCol>
                     </TH>
                     <TH>
-                        <DivFlexCol class="justify-center items-center">
+                        <DivFlexCol>
                             DTS Orders
                             <LabelXS class="text-[10px]">Salmon</LabelXS>
                         </DivFlexCol>
                     </TH>
                     <TH>
-                        <DivFlexCol class="justify-center items-center">
+                        <DivFlexCol>
                             DTS Orders
                             <LabelXS class="text-[10px]"
                                 >Fruits and Vegetables</LabelXS
@@ -44,6 +46,42 @@
                         </DivFlexCol>
                     </TH>
                 </TableHead>
+
+                <TableBody>
+                    <tr>
+                        <TD>SM BICUTAN</TD>
+                        <TD>SMB</TD>
+                        <TD>
+                            <DivFlexCol class="gap-1">
+                                <Badge class="w-fit">SUNDAY</Badge>
+                                <Badge class="w-fit">MONDAY</Badge>
+                                <Badge class="w-fit">TUESDAY</Badge>
+                                <Badge class="w-fit">WEDNESDAY</Badge>
+                            </DivFlexCol>
+                        </TD>
+                        <TD>
+                            <DivFlexCol class="gap-1">
+                                <Badge class="w-fit">THURSDAY</Badge>
+                                <Badge class="w-fit">FRIDAY</Badge>
+                                <Badge class="w-fit">SATURDAY</Badge>
+                            </DivFlexCol>
+                        </TD>
+                        <TD>
+                            <DivFlexCol class="gap-1">
+                                <Badge class="w-fit">MONDAY</Badge>
+                                <Badge class="w-fit">WEDNESDAY</Badge>
+                                <Badge class="w-fit">FRIDAY</Badge>
+                            </DivFlexCol>
+                        </TD>
+                        <TD>
+                            <DivFlexCol class="gap-1">
+                                <Badge class="w-fit">THURSDAY</Badge>
+                                <Badge class="w-fit">SATURDAY</Badge>
+                            </DivFlexCol>
+                        </TD>
+                        <TD>No Schedule</TD>
+                    </tr>
+                </TableBody>
             </Table>
         </TableContainer>
     </Layout>
