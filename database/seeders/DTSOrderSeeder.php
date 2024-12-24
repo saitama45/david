f1fad28738e27b8fdef243c2a9f0497c9d63a071
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-12-24 22:15:51 +0800

    added seeder for salmon

--- database/seeders/DTSOrderSeeder.php
+++ database/seeders/DTSOrderSeeder.php
@@ -40,7 +40,7 @@ class DTSOrderSeeder extends Seeder
     public function run(): void
     {
 
-        $branchDeliverySchedules = [
+        $iceCreamBranchDeliverySchedules = [
             11 => [1, 3, 5],    // GREENFIELD - Mon, Wed, Fri
             7 => [1, 4],        // AYALA MALLS MANILA BAY - Mon, Thu
             15 => [1, 4],       // OKADA - Mon, Thu
@@ -67,12 +67,73 @@ class DTSOrderSeeder extends Seeder
             16 => [3, 6]
         ];
 
-        $startDate = Carbon::create(2024, 9, 1);
+        $salmonBranchDeliverySchedules = [
+            21 => [1, 3, 5],    // Rockwell - Mon, Wed, Fri
+            22 => [2, 4, 6],    // SM Maison - Tue, Thu, Sat
+            23 => [4, 6],       // SM Bicutan - Thu, Sat
+            14 => [2],          // Laus Group Complex - Tue
+            28 => [3],          // The Outlets at Lipa - Wed
+            30 => [1, 3, 5],    // Uptown Mall - Mon, Wed, Fri
+            19 => [1, 3, 5],    // Podium Ortigas - Mon, Wed, Fri
+            10 => [1, 3, 5],    // Glorietta 2 - Mon, Wed, Fri
+            29 => [1, 3, 5],    // Three Central - Mon, Wed, Fri
+            12 => [1, 3, 5],    // High Street South - Mon, Wed, Fri
+            13 => [1, 3, 5],    // KL Tower Serviced Residences - Mon, Wed, Fri
+            20 => [1, 3, 5],    // Robinson's Antipolo - Mon, Wed, Fri
+            6 => [1, 3, 5],     // Robinson's Antipolo - Mon, Wed, Fri
+            11 => [1, 3, 5],    // Greenfield - Mon, Wed, Fri
+            16 => [2, 4, 6],    // NONO'S UP TOWN Center - Tue, Thu, Sat
+            7 => [2, 4, 6],     // NONO'S UP TOWN Center - Tue, Thu, Sat
+            8 => [2, 4, 6],     // Filinvest Super Mall - Tue, Thu, Sat
+            17 => [2, 4, 6],    // Nuvali - Tue, Thu, Sat
+            15 => [2, 4, 6],    // Nono's Okada - Tue, Thu, Sat
+            26 => [2, 4, 6],    // SM Santa Rosa - Tue, Thu, Sat
+            24 => [2, 4, 6],    // SM Fairview Nono's - Tue, Thu, Sat
+            25 => [2, 4, 6],    // SM Grand Central - Tue, Thu, Sat
+            9 => [2, 4, 6],     // Gateway Mall 2 - Tue, Thu, Sat
+            31 => [2, 4, 6],    // Vermosa - Tue, Thu, Sat
+        ];
+        
+
+
+        $startDate = Carbon::create(2024, 11, 4);
         $endDate = Carbon::create(2024, 12, 28);
 
-        $product = ProductInventory::where('inventory_code', '359A2A')->first()->id;
+        $iceCreamId = ProductInventory::where('inventory_code', '359A2A')->first()->id;
+        $salmonId = ProductInventory::where('inventory_code', '269A2A')->first()->id;
 
-        foreach ($branchDeliverySchedules as $branchId => $deliveryDays) {
+        // foreach ($iceCreamBranchDeliverySchedules as $branchId => $deliveryDays) {
+        //     $currentDate = $startDate->copy();
+
+        //     while ($currentDate <= $endDate) {
+        //         if ($currentDate->dayOfWeek !== 0 && in_array($currentDate->dayOfWeek, $deliveryDays)) {
+        //             $storeOrder = StoreOrder::create([
+        //                 'encoder_id' => 1,
+        //                 'supplier_id' => 5,
+        //                 'store_branch_id' => $branchId,
+        //                 'approver_id' => 1,
+        //                 'order_number' => $this->generateOrderNumber($branchId),
+        //                 'order_date' => $currentDate->format('Y-m-d'),
+        //                 'order_status' => 'received',
+        //                 'order_request_status' => 'approved',
+        //                 'type' => 'dts',
+        //             ]);
+
+        //             $quantity = random_int(5, 10);
+        //             $storeOrder->store_order_items()->create([
+        //                 'product_inventory_id' => $iceCreamId,
+        //                 'quantity_ordered' => $quantity,
+        //                 'quantity_approved' => $quantity,
+        //                 'quantity_received' => $quantity,
+        //                 'total_cost' => 109.65 * $quantity
+        //             ]);
+        //         }
+
+        //         $currentDate->addDay();
+        //     }
+        // }
+
+        foreach ($salmonBranchDeliverySchedules as $branchId => $deliveryDays) {
             $currentDate = $startDate->copy();
 
             while ($currentDate <= $endDate) {
@@ -91,7 +152,7 @@ class DTSOrderSeeder extends Seeder
 
                     $quantity = random_int(5, 10);
                     $storeOrder->store_order_items()->create([
-                        'product_inventory_id' => $product,
+                        'product_inventory_id' => $salmonId,
                         'quantity_ordered' => $quantity,
                         'quantity_approved' => $quantity,
                         'quantity_received' => $quantity,
