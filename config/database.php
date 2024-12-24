adddec5d52c8ffeccbeefcd9e5c954b6936ed93c
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-12-25 00:03:27 +0800

    test db max connection error solution

--- config/database.php
+++ config/database.php
@@ -59,7 +59,12 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::ATTR_PERSISTENT => true,
             ]) : [],
+            'pool' => [
+                'min' => 2,
+                'max' => 10,
+            ],
         ],
 
         'mariadb' => [
@@ -147,7 +152,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
