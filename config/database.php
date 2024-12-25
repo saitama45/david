abbdaed51f8d3b3ab81a075fded1b11ec12390a7
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-12-25 09:58:38 +0800

    test db max connection error solution 3

--- config/database.php
+++ config/database.php
@@ -44,6 +44,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
+            'wait_timeout' => 300,
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
@@ -60,6 +61,8 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
                 PDO::ATTR_PERSISTENT => true,
+                PDO::ATTR_EMULATE_PREPARES => true,
+                PDO::ATTR_TIMEOUT => 300
             ]) : [],
             'pool' => [
                 'min_connections' => 1,
