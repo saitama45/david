aad6873405f473375bb71bbf009047d5668bf43f
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-12-31 14:06:37 +0800

    added stock management index page data

--- app/Http/Controllers/StockManagementController.php
+++ app/Http/Controllers/StockManagementController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\ProductInventory;
+use App\Models\UsageRecord;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -10,28 +11,58 @@ class StockManagementController extends Controller
 {
     public function index()
     {
+        $search = request('search');
+        $usageRecords = UsageRecord::with([
+            'usage_record_items.menu.menu_ingredients',
+        ])
+            ->where('store_branch_id', 3)
+            ->get()
+            ->flatMap(function ($usageRecord) {
+                return $usageRecord->usage_record_items->flatMap(function ($item) {
+                    return $item->menu->menu_ingredients->map(function ($ingredient) use ($item) {
+                        return [
+                            'product_id' => $ingredient->product_inventory_id,
+                            'quantity_used' => $ingredient->quantity * $item->quantity
+                        ];
+                    });
+                });
+            })
+            ->groupBy('product_id')
+            ->map(function ($items) {
+                return $items->sum('quantity_used');
+            });
 
-        $products = ProductInventory::with(['inventory_stocks' => function ($query) {
-            $query->where('store_branch_id', 1);
-        }, 'unit_of_measurement'])
+        $query = ProductInventory::query()->with(['inventory_stocks' => function ($query) {
+            $query->where('store_branch_id', 3);
+        }, 'unit_of_measurement']);
+
+        if ($search) {
+            $query->whereAny(['name', 'inventory_code'], 'like', "%$search%");
+        }
+
+        $products = $query
             ->paginate(10)
-            ->through(function ($item) {
+            ->through(function ($item) use ($usageRecords) {
                 return [
+                    'id' => $item->id,
                     'name' => $item->name,
                     'inventory_code' => $item->inventory_code,
                     'stock_on_hand' => $item->inventory_stocks->first()->quantity - $item->inventory_stocks->first()->used,
                     'recorded_used' => $item->inventory_stocks->first()->used,
+                    'estimated_used' => $usageRecords[$item->id] ?? 0,
                     'uom' => $item->unit_of_measurement->name,
                 ];
             });
 
         return Inertia::render('StockManagement/Index', [
-            'products' => $products
+            'products' => $products,
+            'filters' => request()->only(['search'])
         ]);
     }
 
-    public function show()
+    public function show($id)
     {
+
         return Inertia::render('StockManagement/Show');
     }
 }
--- resources/js/Pages/StockManagement/Index.vue
+++ resources/js/Pages/StockManagement/Index.vue
@@ -1,37 +1,54 @@
 <script setup>
+import { useSearch } from "@/Composables/useSearch";
 const { products } = defineProps({
     products: {
         type: Object,
         required: true,
     },
 });
+
+const { search } = useSearch("stock-management.index");
 </script>
 <template>
     <Layout heading="Stock Management">
         <TableContainer>
             <TableHeader>
                 <SearchBar>
-                    <Input class="pl-10" placeholder="Search..." />
+                    <Input
+                        class="pl-10"
+                        placeholder="Search..."
+                        v-model="search"
+                    />
                 </SearchBar>
             </TableHeader>
             <Table>
                 <TableHead>
                     <TH>Name</TH>
                     <TH>Inventory Code</TH>
+                    <TH>UOM</TH>
                     <TH>Stock On Hand</TH>
-                    <TH>Estimated Used</TH>
-                    <TH>Actual Used</TH>
+                    <TH>Sytem Estimated Used</TH>
+                    <TH>Recorded Used</TH>
                     <TH>Actions</TH>
                 </TableHead>
                 <TableBody>
                     <tr v-for="product in products.data">
                         <TD>{{ product.name }}</TD>
                         <TD>{{ product.inventory_code }}</TD>
+                        <TD>{{ product.uom }}</TD>
                         <TD>{{ product.stock_on_hand }}</TD>
-                        <TD> TBD </TD>
+                        <TD>{{ product.estimated_used }}</TD>
                         <TD>{{ product.recorded_used }}</TD>
                         <TD>
-                            <ShowButton />
+                            <ShowButton
+                                :isLink="true"
+                                :href="
+                                    route(
+                                        'stock-management.show',
+                                        product.inventory_code
+                                    )
+                                "
+                            />
                         </TD>
                     </tr>
                 </TableBody>
