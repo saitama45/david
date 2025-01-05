0b5c49c8230b39aaef6189e400a9260d2d03034e
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-01-05 08:04:04 +0800

    hide the add and plus sign on show order details page for not pending orders

--- resources/js/Pages/OrderApproval/Show.vue
+++ resources/js/Pages/OrderApproval/Show.vue
@@ -272,7 +272,7 @@ const addQuantityApproved = (id) => {
             </DivFlexCenter>
 
             <TableHeader>
-                <SearchBar />
+                <!-- <SearchBar /> -->
             </TableHeader>
 
             <Table>
@@ -297,7 +297,10 @@ const addQuantityApproved = (id) => {
                                 itemsDetail.find((item) => item.id === order.id)
                                     ?.quantity_approved || 0
                             }}
-                            <DivFlexCenter class="gap-2">
+                            <DivFlexCenter
+                                v-if="order.order_request_status === 'pending'"
+                                class="gap-2"
+                            >
                                 <button @click="lessQuantityApproved(order.id)">
                                     <Minus class="size-4 text-red-500" />
                                 </button>
