09b6a5809334ca2b42f171f9aaabd01255ed7015
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-01-09 23:33:04 +0800

    added stock on hand column on stock show page

--- resources/js/Pages/Stock/Show.vue
+++ resources/js/Pages/Stock/Show.vue
@@ -34,6 +34,7 @@ const heading = `${product.name} (${product.inventory_code}) Stock Per Store`;
                     <TH>Stock</TH>
                     <TH>Recently Added</TH>
                     <TH>Used</TH>
+                    <TH>Stock On Hand</TH>
                 </TableHead>
                 <TableBody>
                     <tr v-for="item in data.data">
@@ -42,6 +43,7 @@ const heading = `${product.name} (${product.inventory_code}) Stock Per Store`;
                         <TD>{{ item.quantity }}</TD>
                         <TD>{{ item.recently_added }}</TD>
                         <TD>{{ item.used }}</TD>
+                        <TD>{{ item.quantity - item.used }}</TD>
                     </tr>
                 </TableBody>
             </Table>
