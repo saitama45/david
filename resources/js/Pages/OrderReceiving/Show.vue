ce2929fe03499486eed18b181180aaf9282aed95
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-01-22 11:20:55 +0800

    added commited quantity on order receiving show page

--- resources/js/Pages/OrderReceiving/Show.vue
+++ resources/js/Pages/OrderReceiving/Show.vue
@@ -564,7 +564,8 @@ const deleteDeliveryReceiptNumber = (id) => {
                     <TableHead>
                         <TH> Item Code </TH>
                         <TH> Name </TH>
-                        <TH> Quantity Received</TH>
+                        <TH> Commited</TH>
+                        <TH> Received</TH>
                         <TH> Actions </TH>
                     </TableHead>
 
@@ -574,6 +575,7 @@ const deleteDeliveryReceiptNumber = (id) => {
                                 order.product_inventory.inventory_code
                             }}</TD>
                             <TD>{{ order.product_inventory.name }}</TD>
+                            <TD>{{ order.quantity_commited }}</TD>
                             <TD>{{ order.quantity_received }}</TD>
                             <TD class="w-[90px]">
                                 <DivFlexCenter class="gap-1">
