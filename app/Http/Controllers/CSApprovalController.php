63792883ba736489a792b606af45b2a18ef1d2a2
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-01-23 23:32:39 +0800

    removed the die and dump on cs approvabl approve function

--- app/Http/Controllers/CSApprovalController.php
+++ app/Http/Controllers/CSApprovalController.php
@@ -72,6 +72,7 @@ class CSApprovalController extends Controller
 
     public function approve(Request $request)
     {
+        //
         $validated = $request->validate([
             'id' => ['required'],
             'remarks' => ['sometimes'],
