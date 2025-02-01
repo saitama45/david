a458061af07840cda3435ff5068d41589b91008b
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-02-01 09:19:16 +0800

    refractor code on RoleController

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -24,32 +24,16 @@ class RolesController extends Controller
     }
     public function index()
     {
-        $search = request('search');
-        $query = Role::query()->with('permissions');
-
-        if ($search)
-            $query->where('name', 'like', "%$search%");
-        $roles = $query->latest()->paginate(10);
-
         return Inertia::render('Roles/Index', [
-            'roles' => $roles,
+            'roles' => $this->roleService->getRolesList(),
             'filters' => request()->only(['search'])
         ]);
     }
 
-
-    public function edit($id)
+    public function edit(Role $role)
     {
-        $role = Role::with('permissions')->findOrFail($id);
-
-        $groupedPermissions = $this->getPermissionsGroup();
-
-
-        $groupedPermissions = collect($groupedPermissions)->map(function ($permissions) {
-            return $permissions->pluck('name', 'id');
-        });
-
-
+        $role->load(['permissions']);
+        $groupedPermissions = $this->roleService->getPermissionsGroup();
         return Inertia::render('Roles/Edit', [
             'permissions' => $groupedPermissions,
             'role' => $role
@@ -57,16 +41,10 @@ class RolesController extends Controller
     }
 
 
-    public function destroy($id)
+    public function destroy(Role $role)
     {
-        $role = Role::with('users')->findOrFail($id);
-        if ($role->users->count() > 0) {
-            return back()->withErrors([
-                'message' => "Can't delete this role because there are users associated with it."
-            ]);
-        }
         try {
-            $role->delete();
+            $this->roleService->deleteRole($role);
         } catch (Exception $e) {
             return back()->withErrors([
                 'error' => $e->getMessage()
@@ -78,25 +56,18 @@ class RolesController extends Controller
 
     public function create()
     {
-        $groupedPermissions = $this->getPermissionsGroup();
-        $groupedPermissions = collect($groupedPermissions)->map(function ($permissions) {
-            return $permissions->pluck('name', 'id');
-        });
-
+        $groupedPermissions = $this->roleService->getPermissionsGroup();
         return Inertia::render('Roles/Create', [
             'permissions' => $groupedPermissions
         ]);
     }
     public function store(StoreRoleRequest $request)
     {
-        DB::beginTransaction();
         try {
             $this->roleService->createRole($request->validated());
         } catch (Exception $e) {
             return back()->withErrors(['error' => $e->getMessage()]);
         }
-        DB::commit();
-
         return redirect()->route('roles.index')
             ->with('success', 'Role created successfully.');
     }
@@ -104,7 +75,6 @@ class RolesController extends Controller
     public function export(Request $request)
     {
         $search = $request->input('search');
-
         return Excel::download(
             new RolesExport($search),
             'roles-' . now()->format('Y-m-d') . '.xlsx'
@@ -113,15 +83,8 @@ class RolesController extends Controller
 
     public function update(UpdateRoleRequest $request, Role $role)
     {
-        $validated = $request->validated();
-        DB::beginTransaction();
         try {
-            $role->update([
-                'name' => $validated['name']
-            ]);
-            $permissions = Permission::whereIn('id', $validated['selectedPermissions'])->get();
-            $role->syncPermissions($permissions);
-            DB::commit();
+            $this->roleService->updateRole($request->validated(), $role);
         } catch (Exception $e) {
             return back()->withErrors([
                 'error' => $e->getMessage()
@@ -129,86 +92,5 @@ class RolesController extends Controller
         }
         return redirect()->route('roles.index')
             ->with('success', 'Role updated successfully.');
-    }
-
-    public function getPermissionsGroup()
-    {
-        $permissions = Permission::all();
-        return [
-            'user' => $permissions->filter(function ($permission) {
-                return str_contains($permission->name, 'users');
-            }),
-
-            'roles' => $permissions->filter(function ($permission) {
-                return str_contains($permission->name, 'roles');
-            }),
-
-            'dts_delivery_schedules' => $permissions->filter(function ($permission) {
-                return str_contains($permission->name, 'dts delivery schedules');
-            }),
-
-            'store_orders' => $permissions->filter(function ($permission) {
-                return str_contains($permission->name, 'store order');
-            }),
-
-            'dts_orders' => $permissions->filter(function ($permission) {
-                return str_contains($permission->name, 'dts order');
-            }),
-
-            'orders_approval' => $permissions->filter(function ($permission) {
-                return str_contains($permission->name, 'order for approval list') ||
-                    str_contains($permission->name, 'order for approval') ||
-                    str_contains($permission->name, 'approve/decline order request') &&  !str_contains($permission->name, 'cs');
-            }),
-
-            'cs_review_list' => $permissions->filter(function ($permission) {
-                return str_contains($permission->name, 'orders for cs approval') ||
-                    str_contains($permission->name, 'cs approve/decline order request') ||
-                    str_contains($permission->name, 'order for cs approval');
-            }),
-
-            'approved_orders' => $permissions->filter(function ($permission) {
-                return str_contains($permission->name, 'approved order') &&
-                    !str_contains($permission->name, 'for approval');
-            }),
-
-            'approvals' => $permissions->filter(function ($permission) {
-                return str_contains($permission->name, 'received orders for approval') ||
-                    str_contains($permission->name, 'approve received orders') ||
-                    str_contains($permission->name, 'approve image attachments');
-            }),
-
-            'approved_received_items' => $permissions->filter(function ($permission) {
-                return str_contains($permission->name, 'approved received item');
-            }),
-
-            'store_transactions' => $permissions->filter(function ($permission) {
-                return str_contains($permission->name, 'store transaction');
-            }),
-
-            'items' => $permissions->filter(function ($permission) {
-                return !str_contains($permission->name, 'approved received item') && (str_contains($permission->name, 'items') ||
-                    str_contains($permission->name, 'item')) && !str_contains($permission->name, 'order');
-            }),
-
-            'bom' => $permissions->filter(function ($permission) {
-                return str_contains($permission->name, 'bom');
-            }),
-
-            'stock_management' => $permissions->filter(function ($permission) {
-                return str_contains($permission->name, 'stock');
-            }),
-
-            'items_order_summary' => $permissions->filter(function ($permission) {
-                return str_contains($permission->name, 'items order summary') ||
-                    str_contains($permission->name, 'ice cream orders') ||
-                    str_contains($permission->name, 'salmon orders') ||
-                    str_contains($permission->name, 'fruits and vegetables orders');
-            }),
-
-            'manage_references' => $permissions->filter(function ($permission) {
-                return str_contains($permission->name, 'references');
-            })
-        ];
     }
 }
--- app/Http/Services/RoleService.php
+++ app/Http/Services/RoleService.php
@@ -3,6 +3,7 @@
 namespace App\Http\Services;
 
 use App\Models\Role;
+use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Permission;
 
 class RoleService
@@ -10,8 +11,128 @@ class RoleService
 
     public function createRole(array $data)
     {
+        DB::beginTransaction();
         $role = Role::create(['name' => $data['name']]);
         $permissions = Permission::whereIn('id', $data['selectedPermissions'])->get();
         $role->syncPermissions($permissions);
+        DB::commit();
+    }
+
+    public function updateRole(array $data, Role $role)
+    {
+        DB::beginTransaction();
+        $role->update([
+            'name' => $data['name']
+        ]);
+        $permissions = Permission::whereIn('id', $data['selectedPermissions'])->get();
+        $role->syncPermissions($permissions);
+        DB::commit();
+    }
+
+    public function getPermissionsGroup()
+    {
+        $permissions = Permission::all();
+
+        $groupedPermissions = [
+            'user' => $permissions->filter(function ($permission) {
+                return str_contains($permission->name, 'users');
+            }),
+
+            'roles' => $permissions->filter(function ($permission) {
+                return str_contains($permission->name, 'roles');
+            }),
+
+            'dts_delivery_schedules' => $permissions->filter(function ($permission) {
+                return str_contains($permission->name, 'dts delivery schedules');
+            }),
+
+            'store_orders' => $permissions->filter(function ($permission) {
+                return str_contains($permission->name, 'store order');
+            }),
+
+            'dts_orders' => $permissions->filter(function ($permission) {
+                return str_contains($permission->name, 'dts order');
+            }),
+
+            'orders_approval' => $permissions->filter(function ($permission) {
+                return str_contains($permission->name, 'order for approval list') ||
+                    str_contains($permission->name, 'order for approval') ||
+                    str_contains($permission->name, 'approve/decline order request') &&  !str_contains($permission->name, 'cs');
+            }),
+
+            'cs_review_list' => $permissions->filter(function ($permission) {
+                return str_contains($permission->name, 'orders for cs approval') ||
+                    str_contains($permission->name, 'cs approve/decline order request') ||
+                    str_contains($permission->name, 'order for cs approval');
+            }),
+
+            'approved_orders' => $permissions->filter(function ($permission) {
+                return str_contains($permission->name, 'approved order') &&
+                    !str_contains($permission->name, 'for approval');
+            }),
+
+            'approvals' => $permissions->filter(function ($permission) {
+                return str_contains($permission->name, 'received orders for approval') ||
+                    str_contains($permission->name, 'approve received orders') ||
+                    str_contains($permission->name, 'approve image attachments');
+            }),
+
+            'approved_received_items' => $permissions->filter(function ($permission) {
+                return str_contains($permission->name, 'approved received item');
+            }),
+
+            'store_transactions' => $permissions->filter(function ($permission) {
+                return str_contains($permission->name, 'store transaction');
+            }),
+
+            'items' => $permissions->filter(function ($permission) {
+                return !str_contains($permission->name, 'approved received item') && (str_contains($permission->name, 'items') ||
+                    str_contains($permission->name, 'item')) && !str_contains($permission->name, 'order');
+            }),
+
+            'bom' => $permissions->filter(function ($permission) {
+                return str_contains($permission->name, 'bom');
+            }),
+
+            'stock_management' => $permissions->filter(function ($permission) {
+                return str_contains($permission->name, 'stock');
+            }),
+
+            'items_order_summary' => $permissions->filter(function ($permission) {
+                return str_contains($permission->name, 'items order summary') ||
+                    str_contains($permission->name, 'ice cream orders') ||
+                    str_contains($permission->name, 'salmon orders') ||
+                    str_contains($permission->name, 'fruits and vegetables orders');
+            }),
+
+            'manage_references' => $permissions->filter(function ($permission) {
+                return str_contains($permission->name, 'references');
+            })
+        ];
+
+        return collect($groupedPermissions)->map(function ($permissions) {
+            return $permissions->pluck('name', 'id');
+        });
+    }
+
+    public function getRolesList()
+    {
+        $search = request('search');
+        $query = Role::query()->with('permissions');
+
+        if ($search)
+            $query->where('name', 'like', "%$search%");
+        return $query->latest()->paginate(10);
+    }
+
+    public function deleteRole(Role $role)
+    {
+        $role->load(['users']);
+        if ($role->users->count() > 0) {
+            return back()->withErrors([
+                'message' => "Can't delete this role because there are users associated with it."
+            ]);
+        }
+        $role->delete();
     }
 }
