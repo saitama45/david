357bf3d4df79bbca965406cd849b1a2c7ae5384b
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-02-04 22:15:18 +0800

    added cases on OrderStatus page

--- app/Enum/OrderStatus.php
+++ app/Enum/OrderStatus.php
@@ -8,7 +8,9 @@ enum OrderStatus: string
     case INCOMPLETE = 'incomplete';
     case PENDING = 'pending';
     case RECEIVED = 'received';
-
+    case APPROVED = 'approved';
+    case COMMITED = 'commmited';
+    case REJECTED = 'rejected';
     public static function values()
     {
         $data = array_column(self::cases(), 'value');
