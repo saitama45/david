1041046918ad736f8ea69220d67339beafad64be
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-02-24 19:17:21 +0800

    added the data for store transaction approval index table

--- app/Http/Controllers/StoreTransactionApprovalController.php
+++ app/Http/Controllers/StoreTransactionApprovalController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\StoreTransaction;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -9,6 +10,21 @@ class StoreTransactionApprovalController extends Controller
 {
     public function index()
     {
-        return Inertia::render('StoreTransactionApproval/Index');
+        $transactions = StoreTransaction::with('store_transaction_items')
+            ->whereNot('is_approved')
+            ->latest()
+            ->paginate(10)
+            ->withQueryString()
+            ->through(function ($item) {
+                return [
+                    'id' => $item->id,
+                    'receipt_number' => $item->receipt_number,
+                    'order_date' => $item->order_date,
+                    'ordered_item_count' => $item->store_transaction_items->count('quantity')
+                ];
+            });
+        return Inertia::render('StoreTransactionApproval/Index', [
+            'transactions' => $transactions
+        ]);
     }
 }
--- resources/js/Pages/StoreTransactionApproval/Index.vue
+++ resources/js/Pages/StoreTransactionApproval/Index.vue
@@ -1,4 +1,35 @@
-<script setup></script>
+<script setup>
+const { transactions } = defineProps({
+    transactions: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
 <template>
-    <Layout heading="Store Transactions Approval"> </Layout>
+    <Layout heading="Store Transactions Approval">
+        <TableContainer>
+            <Table>
+                <TableHead>
+                    <TH>ID</TH>
+                    <TH>Receipt Number</TH>
+                    <TH>Order Date</TH>
+                    <TH>Ordered Items Count</TH>
+                    <TH>Actions</TH>
+                </TableHead>
+                <TableBody>
+                    <tr v-for="transaction in transactions.data">
+                        <TD>{{ transaction.id }}</TD>
+                        <TD>{{ transaction.receipt_number }}</TD>
+                        <TD>{{ transaction.order_date }}</TD>
+                        <TD>{{ transaction.ordered_item_count }}</TD>
+                        <TD>
+                            <ShowButton />
+                        </TD>
+                    </tr>
+                </TableBody>
+            </Table>
+            <Pagination :data="transactions" />
+        </TableContainer>
+    </Layout>
 </template>
