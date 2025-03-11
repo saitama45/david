72070633837bfcef8632d71b0f12c9d57d3b54eb
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-03-11 10:08:16 +0800

    added table on upcoming inventories index page

--- app/Http/Controllers/UpcomingInventoryController.php
+++ app/Http/Controllers/UpcomingInventoryController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Enum\TimePeriod;
 use App\Models\StoreBranch;
 use App\Models\StoreOrderItem;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -13,19 +15,29 @@ class UpcomingInventoryController extends Controller
     {
         $branches = StoreBranch::options();
         $branchId = request('branchId') ?? $branches->keys()->first();
+        $timePeriods = TimePeriod::values();
+        $time_period = request('time_period') ?? $timePeriods[1];
 
-        $inventories = StoreOrderItem::with(['store_order', 'product_inventory'])
+        $query = StoreOrderItem::with(['store_order', 'product_inventory'])
             ->whereHas('store_order', function ($query) use ($branchId) {
-                $query->where('store_branch_id', 16);
+                $query->where('store_branch_id', $branchId);
                 $query->where('order_status', 'commited');
-            })
-            ->latest()
+            });
+
+        // if ($time_period != 0) {
+        //     $query->whereMonth('store_orders.order_date', $time_period);
+        // } else {
+        //     $query->whereYear('store_orders.order_date', Carbon::today()->year);
+        // }
+
+        $inventories = $query->latest()
             ->paginate(10);
 
-
-
         return Inertia::render('UpcomingInventories/Index', [
-            'inventories' => $inventories
+            'inventories' => $inventories,
+            'branches' => $branches,
+            'filters' => request()->only(['from', 'to', 'branchId', 'search']),
+            'timePeriods' => $timePeriods
         ]);
     }
 }
