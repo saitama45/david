61acb2d24c3cb8ab9ba5abb09bafce916c2318f7
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-03-17 19:57:54 +0800

    added inventories report filter

--- app/Http/Controllers/InventoryReportController.php
+++ app/Http/Controllers/InventoryReportController.php
@@ -2,13 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Enum\TimePeriod;
+use App\Models\ProductInventoryStockManager;
+use App\Models\StoreBranch;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class InventoryReportController extends Controller
 {
     public function index()
     {
-        return Inertia::render('InventoryReport/Index');
+        $branches = StoreBranch::options();
+        $branchId = request('branchId') ?? $branches->keys()->first();
+        $timePeriods = TimePeriod::values();
+        $time_period = request('time_period') ?? $timePeriods[1];
+
+        $query = ProductInventoryStockManager::query();
+
+        if ($time_period != 0) {
+            $query->whereMonth('transaction_date', '<=', $time_period);
+        } else {
+            $query->whereYear('transaction_date', Carbon::today()->year);
+        }
+
+        $query->select(
+            'product_inventory_id',
+            DB::raw('SUM(quantity) as total_quantity'),
+            DB::raw('SUM(total_cost) as total_cost')
+        )
+            ->with('product')
+            ->where('store_branch_id', $branchId)
+            ->groupBy('product_inventory_id');
+
+        $inventories = $query->paginate(10);
+
+        $summarizedInventories = $inventories->through(function ($item) {
+            return [
+                'quantity' => $item->total_quantity,
+                'total_cost' => number_format($item->total_cost, 2, '.', ','),
+                'item' => $item->product->name,
+                'inventory_code' => $item->product->inventory_code
+            ];
+        });
+
+        return Inertia::render('InventoryReport/Index', [
+            'inventories' => $summarizedInventories,
+            'branches' => $branches,
+            'filters' => request()->only(['from', 'to', 'branchId', 'search']),
+            'timePeriods' => $timePeriods
+        ]);
     }
 }
--- resources/js/Pages/InventoryReport/Index.vue
+++ resources/js/Pages/InventoryReport/Index.vue
@@ -1,7 +1,135 @@
-<script setup></script>
+<script setup>
+import { useSelectOptions } from "@/Composables/useSelectOptions";
+import { usePage } from "@inertiajs/vue3";
+import { throttle } from "lodash";
+import { router } from "@inertiajs/vue3";
+const { inventories, branches, timePeriods, filters } = defineProps({
+    inventories: {
+        type: Object,
+        required: true,
+    },
+    branches: {
+        type: Object,
+        required: true,
+    },
+    timePeriods: {
+        type: Object,
+        required: true,
+    },
+    filters: {
+        type: Object,
+        required: true,
+    },
+});
+
+const { options: branchesOptions } = useSelectOptions(branches);
+let search = ref(usePage().props.filters.search);
+const branchId = ref(filters.branchId || branchesOptions.value[0].value);
+
+const { options: timePeriodOptions } = useSelectOptions(timePeriods);
+const time_period = ref(
+    filters.time_period || timePeriodOptions.value[0].value
+);
+watch(branchId, (value) => {
+    router.get(
+        route("inventories-report.index"),
+        {
+            search: search.value,
+            branchId: value,
+            time_period: time_period.value,
+        },
+        {
+            preserveState: true,
+            preserveScroll: true,
+        }
+    );
+});
+
+watch(time_period, (value) => {
+    router.get(
+        route("inventories-report.index"),
+        {
+            search: search.value,
+            time_period: value,
+            branchId: branchId.value,
+        },
+        {
+            preserveState: true,
+            preserveScroll: true,
+        }
+    );
+});
+
+watch(
+    search,
+    throttle(function (value) {
+        router.get(
+            route("inventories-report.index"),
+            {
+                search: value,
+                branchId: branchId.value,
+                time_period: time_period.value,
+            },
+            {
+                preserveState: true,
+                replace: true,
+            }
+        );
+    }, 500)
+);
+</script>
 
 <template>
-     <Layout heading="Inventories">
-        
-     </Layout>
+    <Layout heading="Inventories">
+        <TableContainer>
+            <TableHeader>
+                <SearchBar>
+                    <Input
+                        class="pl-10"
+                        placeholder="Search..."
+                        v-model="search"
+                    />
+                </SearchBar>
+
+                <DivFlexCenter class="gap-5">
+                    <Select
+                        filter
+                        placeholder="Select a Supplier"
+                        v-model="branchId"
+                        :options="branchesOptions"
+                        optionLabel="label"
+                        optionValue="value"
+                    >
+                    </Select>
+                    <InputContainer>
+                        <Select
+                            v-model="time_period"
+                            filter
+                            placeholder="Time Periods"
+                            :options="timePeriodOptions"
+                            optionLabel="label"
+                            optionValue="value"
+                        ></Select>
+                    </InputContainer>
+                </DivFlexCenter>
+            </TableHeader>
+            <Table>
+                <TableHead>
+                    <TH>Item</TH>
+                    <TH>Inventory Code</TH>
+                    <TH>Quantity</TH>
+                    <TH>Total Cost</TH>
+                </TableHead>
+                <TableBody>
+                    <tr v-for="inventory in inventories.data">
+                        <TD>{{ inventory.item }}</TD>
+                        <TD>{{ inventory.inventory_code }}</TD>
+                        <TD>{{ inventory.quantity }}</TD>
+                        <TD>{{ inventory.total_cost }}</TD>
+                    </tr>
+                </TableBody>
+            </Table>
+            <Pagination :data="inventories" />
+        </TableContainer>
+    </Layout>
 </template>
