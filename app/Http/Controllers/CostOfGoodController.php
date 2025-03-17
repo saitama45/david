a632538eff3fb506ae16766e00a29a0283dcf668
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-03-17 11:00:53 +0800

    added cost of goods page filters

--- app/Http/Controllers/CostOfGoodController.php
+++ app/Http/Controllers/CostOfGoodController.php
@@ -2,13 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Enum\TimePeriod;
+use App\Models\ProductInventoryStockManager;
+use App\Models\StoreBranch;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Inertia\Inertia;
 
 class CostOfGoodController extends Controller
 {
     public function index()
     {
-        return Inertia::render('CostOfGood/Index');
+        $branches = StoreBranch::options();
+        $branchId = request('branchId') ?? $branches->keys()->first();
+        $search = request('search');
+        $timePeriods = TimePeriod::values();
+        $time_period = request('time_period') ?? $timePeriods[1];
+
+        $query = ProductInventoryStockManager::with('cost_center')
+            ->where('store_branch_id', $branchId)
+            ->where('total_cost', '<', 0);
+
+        if ($time_period != 0) {
+            $query->whereMonth('transaction_date', $time_period);
+        } else {
+            $query->whereYear('transaction_date', Carbon::today()->year);
+        }
+        
+        $costOfGoods = $query->latest()
+            ->paginate(10);
+        return Inertia::render('CostOfGood/Index', [
+            'costOfGoods' => $costOfGoods,
+            'filters' => request()->only(['from', 'to', 'branchId', 'search', 'time_period']),
+            'timePeriods' => $timePeriods,
+            'branches' => $branches
+        ]);
     }
 }
--- resources/js/Pages/CostOfGood/Index.vue
+++ resources/js/Pages/CostOfGood/Index.vue
@@ -1,9 +1,141 @@
-<script setup></script>
+<script setup>
+import { useSelectOptions } from "@/Composables/useSelectOptions";
+import { usePage } from "@inertiajs/vue3";
+import { throttle } from "lodash";
+import { router } from "@inertiajs/vue3";
+const { costOfGoods, branches, timePeriods, filters } = defineProps({
+    
+    costOfGoods: {
+        type: Object,
+        required: true,
+    },
+    branches: {
+        type: Object,
+        required: true,
+    },
+    timePeriods: {
+        type: Object,
+        required: true,
+    },
+    filters: {
+        type: Object,
+        required: true,
+    },
+});
+
+const { options: branchesOptions } = useSelectOptions(branches);
+let search = ref(usePage().props.filters.search);
+const branchId = ref(filters.branchId || branchesOptions.value[0].value);
+
+const { options: timePeriodOptions } = useSelectOptions(timePeriods);
+const time_period = ref(
+    filters.time_period || timePeriodOptions.value[0].value
+);
+
+watch(branchId, (value) => {
+    router.get(
+        route("cost-of-goods.index"),
+        {
+            search: search.value,
+            branchId: value,
+            time_period: time_period.value,
+        },
+        {
+            preserveState: true,
+            preserveScroll: true,
+        }
+    );
+});
+
+watch(time_period, (value) => {
+    router.get(
+        route("cost-of-goods.index"),
+        {
+            search: search.value,
+            time_period: value,
+            branchId: branchId.value,
+        },
+        {
+            preserveState: true,
+            preserveScroll: true,
+        }
+    );
+});
+
+watch(
+    search,
+    throttle(function (value) {
+        router.get(
+            route("cost-of-goods.index"),
+            {
+                search: value,
+                branchId: branchId.value,
+                time_period: time_period.value,
+            },
+            {
+                preserveState: true,
+                replace: true,
+            }
+        );
+    }, 500)
+);
+</script>
 
 <template>
-    <Layout
-        heading="Cost of Goods"
-        >
-        
-        </Layout>
+    <Layout heading="Cost of Goods">
+        <TableContainer>
+            <TableHeader>
+                <SearchBar>
+                    <Input
+                        class="pl-10"
+                        placeholder="Search..."
+                        v-model="search"
+                    />
+                </SearchBar>
+
+                <DivFlexCenter class="gap-5">
+                    <Select
+                        filter
+                        placeholder="Select a Supplier"
+                        v-model="branchId"
+                        :options="branchesOptions"
+                        optionLabel="label"
+                        optionValue="value"
+                    >
+                    </Select>
+                    <InputContainer>
+                        <Select
+                            v-model="time_period"
+                            filter
+                            placeholder="Time Periods"
+                            :options="timePeriodOptions"
+                            optionLabel="label"
+                            optionValue="value"
+                        ></Select>
+                    </InputContainer>
+                </DivFlexCenter>
+            </TableHeader>
+            <Table>
+                <TableHead>
+                    <TH>ID</TH>
+                    <TH>Quantity</TH>
+                    <TH>Cost Center</TH>
+                    <TH>Unit Cost</TH>
+                    <TH>Total Cost</TH>
+                    <TH>Transaction Date</TH>
+                </TableHead>
+                <TableBody>
+                    <tr v-for="costOfGood in costOfGoods.data">
+                        <TD>{{ costOfGood.id }}</TD>
+                        <TD>{{ costOfGood.quantity }}</TD>
+                        <TD>{{ costOfGood.cost_center?.name ?? "N/a" }}</TD>
+                        <TD>{{ costOfGood.unit_cost }}</TD>
+                        <TD>{{ costOfGood.total_cost }}</TD>
+                        <TD>{{ costOfGood.transaction_date }}</TD>
+                    </tr>
+                </TableBody>
+            </Table>
+            <Pagination :data="costOfGoods" />
+        </TableContainer>
+    </Layout>
 </template>
