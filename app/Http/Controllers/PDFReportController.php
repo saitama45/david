1276d384b80c749e1cac29f3766e452903309a38
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-04-04 10:28:13 +0800

    made sales report pdf data dyanamic

--- app/Http/Controllers/PDFReportController.php
+++ app/Http/Controllers/PDFReportController.php
@@ -7,6 +7,7 @@ use App\Models\StoreOrder;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class PDFReportController extends Controller
 {
@@ -19,15 +20,24 @@ class PDFReportController extends Controller
     {
         $validated = $request->only('branch', 'start_date', 'end_date');
 
+        $start_date = Carbon::parse($validated['start_date'])->addDay()->format('Y-m-d');
+        $end_date = Carbon::parse($validated['end_date'])->addDay()->format('Y-m-d');
 
         $orders = StoreOrder::with('supplier')
             ->where('store_branch_id', $validated['branch'])
-            ->whereBetween('order_date', [Carbon::parse($validated['start_date'])->format('Y-m-d'), Carbon::parse($validated['end_date'])->format('Y-m-d')])
+            ->whereBetween('order_date', [$start_date, $end_date])
             ->get();
 
         $pdf = Pdf::loadView('pdf.store-orders-report', [
-            'branch' => StoreBranch::findOrFail(4)->name,
-            'orders' => $orders
+            'branch' => StoreBranch::findOrFail($validated['branch'])->name,
+            'orders' => $orders,
+            'start_date' => $start_date,
+            'end_date' => $end_date,
+            'date_generated' => Carbon::now()->format('Y-m-d H:i:s'),
+            'generated_by' => Auth::user()->full_name,
+            'pending' => $orders->where('order_status', 'pending')->count(),
+            'approved' => $orders->where('order_status', 'approved')->count(),
+            'commited' => $orders->where('order_status', 'commited')->count(),
         ]);
 
         return $pdf->setPaper('legal', 'landscape')->download('store-orders.pdf');
