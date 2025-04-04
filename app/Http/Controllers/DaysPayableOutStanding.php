62f5f77713bcdea7525d9a97eec6a4f9e4c8d572
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-04-04 21:42:33 +0800

    updated dpo list

--- app/Http/Controllers/DaysPayableOutStanding.php
+++ app/Http/Controllers/DaysPayableOutStanding.php
@@ -2,13 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProductInventoryStockManager;
+use App\Models\StoreBranch;
+use App\Models\StoreOrderItem;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class DaysPayableOutStanding extends Controller
 {
     public function index()
     {
-        return Inertia::render('DaysPayableOutstanding/Index');
+        $branches = StoreBranch::options();
+        $branchId = request('branchId') ?? $branches->keys()->first();
+        $chart_time_period = request('chart_time_period') ?? 0;
+
+        $accountPayableAll = StoreOrderItem::query()
+            ->join('store_orders', 'store_order_items.store_order_id', '=', 'store_orders.id')
+            ->join('product_inventories', 'store_order_items.product_inventory_id', '=', 'product_inventories.id')
+            ->where('store_orders.store_branch_id', $branchId)
+            ->where('store_order_items.quantity_received', '>', 0)
+            ->sum(DB::raw('store_order_items.quantity_received * product_inventories.cost'));
+
+        $cogsAll = ProductInventoryStockManager::where('store_branch_id', $branchId)
+            ->where('total_cost', '<', 0)->sum(DB::raw('ABS(total_cost)'));
+
+        return Inertia::render('DaysPayableOutstanding/Index', [
+            'filters' => request()->only(['branchId', 'search', 'chart_time_period']),
+            'branches' => $branches,
+            'accountPayable' => number_format($accountPayableAll, 2, '.', ','),
+            'costOfGoods' => number_format($cogsAll, 2, '.', ',')
+        ]);
     }
 }
--- resources/js/Pages/DaysPayableOutstanding/Index.vue
+++ resources/js/Pages/DaysPayableOutstanding/Index.vue
@@ -1,5 +1,69 @@
-<script setup></script>
+<script setup>
+import { useSelectOptions } from "@/Composables/useSelectOptions";
+import { router } from "@inertiajs/vue3";
+const { branches, filters } = defineProps({
+    branches: {
+        type: Object,
+        required: true,
+    },
+    filters: {
+        type: Object,
+        required: true,
+    },
+    accountPayable: String,
+    costOfGoods: String,
+});
+const { options: branchesOptions } = useSelectOptions(branches);
+const branchId = ref(filters.branchId || branchesOptions.value[0].value);
+
+watch(branchId, (value) => {
+    router.get(
+        route("days-payable-outstanding.index"),
+        {
+            branchId: value,
+        },
+        {
+            preserveState: true,
+            preserveScroll: true,
+        }
+    );
+});
+</script>
 
 <template>
-    <Layout heading="Days Payable Outstanding"></Layout>
+    <Layout heading="Days Payable Outstanding">
+        <TableContainer>
+            <TableHeader>
+                <span></span>
+
+                <DivFlexCenter class="gap-5">
+                    <Select
+                        filter
+                        placeholder="Select a Branch"
+                        v-model="branchId"
+                        :options="branchesOptions"
+                        optionLabel="label"
+                        optionValue="value"
+                    >
+                    </Select>
+                </DivFlexCenter>
+            </TableHeader>
+            <Table>
+                <TableHead>
+                    <TH>Particular</TH>
+                    <TH>Value</TH>
+                </TableHead>
+                <TableBody>
+                    <tr>
+                        <TD>Account Payable</TD>
+                        <TD>{{ accountPayable }}</TD>
+                    </tr>
+                    <tr>
+                        <TD>Cost of Goods Sold</TD>
+                        <TD>{{ costOfGoods }}</TD>
+                    </tr>
+                </TableBody>
+            </Table>
+        </TableContainer>
+    </Layout>
 </template>
