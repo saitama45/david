2e0a6299b72a3e17182a1ff1670a5ceca3c8ab74
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2025-05-30 21:33:50 +0800

    added codes on filesystems

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,15 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
+        'public_file' => [
+            'driver' => 'local',
+            'root' => storage_path('storage'),
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
