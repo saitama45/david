594b65f7af4dea514cf9be8afe1513d9c2a2a713
unknown <genermagbanua45@gmail.com>
2025-08-04 10:47:38 +0800

    Include download template in StoreOrder Create vue

--- resources/js/Pages/StoreOrder/Create.vue
+++ resources/js/Pages/StoreOrder/Create.vue
@@ -1063,38 +1063,46 @@ watch(orderForm, (value) => {
             <DialogContent class="sm:max-w-[600px]">
                 <DialogHeader>
                     <DialogTitle>Import Orders</DialogTitle>
-                    <DialogDescription
-                        >Upload an Excel file to import orders.</DialogDescription
-                    >
+                    <DialogDescription>
+                        Upload an Excel file to import orders.
+                    </DialogDescription>
                 </DialogHeader>
-                <div class="grid gap-4 py-4">
-                    <InputContainer>
-                        <Label for="orders_file">Excel File</Label>
+                <div class="space-y-5">
+                    <div class="flex flex-col space-y-1">
                         <Input
-                            id="orders"
                             type="file"
-                            @change="
-                                excelFileForm.orders_file = $event.target.files[0]
+                            @input="
+                                (event) =>
+                                    (excelFileForm.orders_file =
+                                        event.target.files[0])
                             "
                         />
                         <FormError>{{
                             excelFileForm.errors.orders_file
                         }}</FormError>
-                    </InputContainer>
+                    </div>
+                    <div class="flex flex-col space-y-1">
+                        <Label class="text-xs">Accepted Orders File Format</Label>
+                        <ul>
+                            <li class="text-xs">
+                                <a
+                                    class="text-blue-500 underline"
+                                    :href="route('excel.store-order-template')"
+                                    >Click to download template</a
+                                >
+                            </li>
+                        </ul>
+                    </div>
                 </div>
                 <DialogFooter>
-                    <Button variant="ghost" @click="visible = false"
-                        >Cancel</Button
+                    <Button
+                        :disabled="isLoading"
+                        @click="addImportedItemsToOrderList"
+                        type="submit"
+                        class="gap-2"
                     >
-                    <Button @click="addImportedItemsToOrderList" :disabled="isLoading">
-                        <div v-if="isLoading" class="flex items-center">
-                            <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                            </svg>
-                            Importing...
-                        </div>
-                        <span v-else>Import</span>
+                        Import
+                        <span><Loading v-if="isLoading" /></span>
                     </Button>
                 </DialogFooter>
             </DialogContent>
