58d022c1ae0bbb06d37f3e19db1b473a29443d48
unknown <genermagbanua45@gmail.com>
2025-08-04 09:20:21 +0800

    Upload Excel Template for BOM

--- app/Http/Controllers/ExcelTemplateController.php
+++ app/Http/Controllers/ExcelTemplateController.php
@@ -67,6 +67,24 @@ class ExcelTemplateController extends Controller
         return response()->download($path);
     }
 
+    public function posBomTemplate()
+    {
+        // Define the path to your new template file
+        // Use `storage_path('app/public/excel-templates/sapmasterfile_template.xlsx')`
+        // for a more robust path that works across different OS.
+        $path = 'storage\excel-templates\BOM_template.xlsx';
+
+        // Check if the file exists before attempting to download
+        if (!file_exists($path)) {
+            // Log an error or return a 404 response if the file is not found
+            // For production, you might want a more user-friendly error page.
+            abort(404, 'BOM template file not found.');
+        }
+
+        // Return the download response
+        return response()->download($path);
+    }
+
     public function SupplierItemsTemplate()
     {
         // Define the path to your new template file
