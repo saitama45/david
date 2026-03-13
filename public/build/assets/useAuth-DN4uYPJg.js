826b934dd5b3403cf546181657a94f1a4da01214
saitama45 <genermagbanua45@gmail.com>
2026-03-13 08:55:49 +0800

    Final normalization of casing for all components and pages to fix Linux build

--- public/build/assets/useAuth-DN4uYPJg.js
+++
@@ -1,1 +0,0 @@
-import{Q as e}from"./app-rjK1ng7o.js";function t(){const s=e().props.auth.permissions;return{hasAccess:r=>s.includes(r)}}export{t as u};
--- public/build/assets/useToast-DBCD-RTl.js
+++
@@ -1,1 +0,0 @@
-import{G as t}from"./app-rjK1ng7o.js";function a(){return{toast:t()}}export{a as u};
