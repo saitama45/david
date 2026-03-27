cdd5aa49aa18d3243632388e36c9da964c7beccf
saitama45 <genermagbanua45@gmail.com>
2026-03-28 01:02:02 +0800

    fix: Correct the Move error from redis in Azure server

--- config/database.php
+++ config/database.php
@@ -163,10 +163,9 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
-            'persistent' => env('REDIS_PERSISTENT', false),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
             'context' => [
-                'ssl' => [
+                'stream' => [
                     'verify_peer' => false,
                     'verify_peer_name' => false,
                 ],
@@ -174,33 +173,23 @@ return [
         ],
 
         'default' => [
-            'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
             'scheme' => env('REDIS_SCHEME', 'tcp'),
-            'ssl' => env('REDIS_SCHEME') === 'tls' ? ['verify_peer' => false, 'verify_peer_name' => false] : null,
-            'max_retries' => env('REDIS_MAX_RETRIES', 3),
-            'backoff_algorithm' => env('REDIS_BACKOFF_ALGORITHM', 'decorrelated_jitter'),
-            'backoff_base' => env('REDIS_BACKOFF_BASE', 100),
-            'backoff_cap' => env('REDIS_BACKOFF_CAP', 1000),
+            'ssl' => ['verify_peer' => false, 'verify_peer_name' => false],
         ],
 
         'cache' => [
-            'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'database' => env('REDIS_CACHE_DB', '0'),
             'scheme' => env('REDIS_SCHEME', 'tcp'),
-            'ssl' => env('REDIS_SCHEME') === 'tls' ? ['verify_peer' => false, 'verify_peer_name' => false] : null,
-            'max_retries' => env('REDIS_MAX_RETRIES', 3),
-            'backoff_algorithm' => env('REDIS_BACKOFF_ALGORITHM', 'decorrelated_jitter'),
-            'backoff_base' => env('REDIS_BACKOFF_BASE', 100),
-            'backoff_cap' => env('REDIS_BACKOFF_CAP', 1000),
+            'ssl' => ['verify_peer' => false, 'verify_peer_name' => false],
         ],
 
     ],
